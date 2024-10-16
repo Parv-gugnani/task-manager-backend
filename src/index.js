6816d0c04bfdef3755db358a1ddefe8a5950163e
Parv-gugnani <9765.parv@gmail.com>
2024-10-16 13:44:17 +0530

    task endpoint

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ require("./db/mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const Task = require("./models/task");
+
 app.use(express.json());
 
 app.post("/users", (req, res) => {
@@ -19,6 +21,28 @@ app.post("/users", (req, res) => {
       res.send(e);
     });
 });
+
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => {
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+      res.send(e);
+    });
+});
+
+//Goal : Setup the task creation endpoint
+
+//1: create seprate file for the task model
+//2: create the task creation endpoint (handle sucess and error)
+//3: test the endpoint from postMan
+
+//completed
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
