e626665b2262dffef973cfaaaf8eef6e77fb551f
Parv-gugnani <9765.parv@gmail.com>
2024-10-19 17:09:22 +0530

    router

--- src/index.js
+++ src/index.js
@@ -3,93 +3,14 @@ const User = require("./models/user");
 require("./db/mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
+const userRouter = require("../src/routers/userRouter");
+const taskRouter = require("../src/routers/taskRouter");
 
 const Task = require("./models/task");
 
 app.use(express.json());
-
-app.post("/users", async (req, res) => {
-  const user = new User(req.body);
-
-  try {
-    await user.save();
-    res.status(201).send(user);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-app.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findById(_id);
-
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    res.send(user);
-  } catch (e) {
-    res.status(500).send();
-  }
-
-  // User.findById(_id)
-  //   .then((user) => {
-  //     if (!user) {
-  //       return res.status(404).send();
-  //     }
-  //     res.send(user);
-  //   })
-  //   .catch((e) => {
-  //     res.status(500).send();
-  //   });
-  // console.log(req.params);
-});
-
-app.post("/tasks", async (req, res) => {
-  const task = new Task(req.body);
-
-  try {
-    await task.save();
-    res.status(201).send(task);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-app.get("/tasks", async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.send(tasks);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-app.get("/tasks/:id", async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const task = await Task.findById(_id);
-
-    if (!task) {
-      return res.status(404).send();
-    }
-
-    res.send(task);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
+app.use(userRouter);
+app.use(taskRouter);
 
 //Goal : Setup the task creation endpoint
 
