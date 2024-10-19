f1c261ba206cf580ed685250a445c4789b4ca46f
Parv-gugnani <9765.parv@gmail.com>
2024-10-19 13:19:46 +0530

    async

--- src/index.js
+++ src/index.js
@@ -8,88 +8,87 @@ const Task = require("./models/task");
 
 app.use(express.json());
 
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const user = new User(req.body);
 
-  user
-    .save()
-    .then(() => {
-      res.send(user);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-      res.send(e);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
-app.get("/users", (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.send(users);
-    })
-    .catch((e) => {
-      res.status(500).send();
-    });
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
-app.get("/users/:id", (req, res) => {
+app.get("/users/:id", async (req, res) => {
   const _id = req.params.id;
+  try {
+    const user = await User.findById(_id);
 
-  User.findById(_id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send();
-      }
+    if (!user) {
+      return res.status(404).send();
+    }
 
-      res.send(user);
-    })
-    .catch((e) => {
-      res.status(500).send();
-    });
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
 
-  console.log(req.params);
+  // User.findById(_id)
+  //   .then((user) => {
+  //     if (!user) {
+  //       return res.status(404).send();
+  //     }
+  //     res.send(user);
+  //   })
+  //   .catch((e) => {
+  //     res.status(500).send();
+  //   });
+  // console.log(req.params);
 });
 
-app.post("/tasks", (req, res) => {
+app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
 
-  task
-    .save()
-    .then(() => {
-      res.send(task);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-      res.send(e);
-    });
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
-app.get("/tasks", (req, res) => {
-  Task.find({})
-    .then((task) => {
-      res.send(task);
-    })
-    .catch((e) => {
-      res.status(500).send();
-    });
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
-app.get("/tasks/:id", (req, res) => {
+app.get("/tasks/:id", async (req, res) => {
   const _id = req.params.id;
 
-  Task.findById(_id)
-    .then((task) => {
-      if (!task) {
-        return res.status(404).send();
-      }
+  try {
+    const task = await Task.findById(_id);
 
-      res.send(task);
-    })
-    .catch((e) => {
-      res.status(500).send();
-    });
+    if (!task) {
+      return res.status(404).send();
+    }
 
-  console.log(req.params);
+    res.send(task);
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
 //Goal : Setup the task creation endpoint
